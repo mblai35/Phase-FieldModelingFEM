8245365a1ab791af86a3c8ca6aa656fe587422dd
Xiukun Hu <xhu4@uwyo.edu>
2016-12-02 16:20:13 -0700

    fem2d_phasefield_linear.m works!

--- fem2d_phasefield_linear.m
+++ fem2d_phasefield_linear.m
@@ -1,4 +1,4 @@
-function fem2d_phasefield_linear ( nx, ny )
+function fem2d_phasefield_linear ( nx, ny, dt )
 
 %*****************************************************************************80
 %
@@ -85,14 +85,13 @@ function fem2d_phasefield_linear ( nx, ny )
 %
 %    Local, real YT, the Y coordinate of the top boundary.
 %
-  timestamp ( );
+%   timestamp ( );
 
   %%
-  dt = .0001;
   phi = zeros(nx,ny);
   phi(round(nx/3):floor(nx/3*2),round(ny/3):floor(ny/3*2)) = 1;
   phi = reshape(phi',[],1);
-  nt = 1000;
+  nt = 1/dt;
   
   
   %%
@@ -307,8 +306,11 @@ function fem2d_phasefield_linear ( nx, ny )
 %  If the K-th variable is at a boundary node, replace the K-th finite
 %  element equation by a boundary condition that sets the variable to U(K).
 %
+
+surf(reshape(phi',nx,ny));
+
 for it = 0:nt
-b = a * phi;
+b = -a * phi;
 dphidt = c \ b;
 nphi = dt * dphidt + phi;
 k = 0;
@@ -332,327 +334,329 @@ k = 0;
       end
   end
   figure(1);
-  surf(reshape(phi',nx,ny));
-  pause(.1);
-phi = nphi;
+  if (rem(it*dt,.0001)==0)
+      surf(reshape(phi',nx,ny));
+      drawnow;
+  end
+  phi = nphi;
 end
 %
-%  SOLVE the linear system A * C = B.
-%
-  c = a \ b;
-%
-%  COMPARE computed and exact solutions at the nodes.
-%
-  fprintf ( 1, '\n' );
-  fprintf ( 1, '     K     I     J          X           Y        U               U\n' );
-  fprintf ( 1, '                                                 exact           computed \n' );
-
-  k = 0;
-
-  for j = 1 : ny
-    fprintf ( 1, '\n' );
-    for i = 1 : nx
-
-      k = k + 1;
-
-      [ u, dudx, dudy ] = exact ( node_xy(1,k), node_xy(2,k) );
-
-      fprintf ( 1, '  %4d  %4d  %4d  %10f  %10f  %14e  %14e  %14e\n', ...
-        k, i, j, node_xy(1,k), node_xy(2,k), u, c(k), abs ( u - c(k) ) );
-
-    end
-
-  end
-%
-%  Compute error integrals.
-%
-  if ( 1 )
-
-  el2 = 0.0;
-  eh1 = 0.0;
-
-  for e = 1 : element_num
-
-    i1 = element_node(1,e);
-    i2 = element_node(2,e);
-    i3 = element_node(3,e);
-
-    area = 0.5 * ...
-      ( node_xy(1,i1) * ( node_xy(2,i2) - node_xy(2,i3) ) ...
-      + node_xy(1,i2) * ( node_xy(2,i3) - node_xy(2,i1) ) ...
-      + node_xy(1,i3) * ( node_xy(2,i1) - node_xy(2,i2) ) );
-%
-%  Consider each quadrature point.
-%  Here, we use the midside nodes as quadrature points.
-%
-    for q1 = 1 : 3
-
-      q2 = mod ( q1, 3 ) + 1;
-
-      nq1 = element_node(q1,e);
-      nq2 = element_node(q2,e);
-
-      xq = 0.5 * ( node_xy(1,nq1) + node_xy(1,nq2) );
-      yq = 0.5 * ( node_xy(2,nq1) + node_xy(2,nq2) );
-      wq = 1.0 / 3.0;
-
-      uh = 0.0;
-      dudxh = 0.0;
-      dudyh = 0.0;
-
-      for tj1 = 1 : element_order
-
-        tj2 = mod ( tj1,     3 ) + 1;
-        tj3 = mod ( tj1 + 1, 3 ) + 1;
-
-        ntj1 = element_node(tj1,e);
-        ntj2 = element_node(tj2,e);
-        ntj3 = element_node(tj3,e);
-
-        qj = 0.5 * ( ...
-            ( node_xy(1,ntj3) - node_xy(1,ntj2) ) * ( yq - node_xy(2,ntj2) ) ...
-          - ( node_xy(2,ntj3) - node_xy(2,ntj2) ) * ( xq - node_xy(1,ntj2) ) ) ...
-              / area;
-        dqjdx = - 0.5 * ( node_xy(2,ntj3) - node_xy(2,ntj2) ) / area;
-        dqjdy =   0.5 * ( node_xy(1,ntj3) - node_xy(1,ntj2) ) / area;
-
-        uh    = uh    + c(ntj1) * qj;
-        dudxh = dudxh + c(ntj1) * dqjdx;
-        dudyh = dudyh + c(ntj1) * dqjdy;
-
-      end
-
-      [ u, dudx, dudy ] = exact ( xq, yq );
-
-      el2 = el2 + ( uh - u )^2 * area;
-      eh1 = eh1 + ( ( dudxh - dudx )^2 + ( dudyh - dudy )^2 ) * area;
-
-    end
-
-  end
-
-  el2 = sqrt ( el2 );
-  eh1 = sqrt ( eh1 );
-
-  fprintf ( 1, '\n' );
-  fprintf ( 1, '*********************************************\n' );
-  fprintf ( 1, '*                                           *\n' );
-  fprintf ( 1, '*  ERRORS:                                  *\n' );
-  fprintf ( 1, '*    L2 error =          %14f     *\n', el2 );
-  fprintf ( 1, '*    H1-seminorm error = %14f     *\n', eh1 );
-  fprintf ( 1, '*                                           *\n' );
-  fprintf ( 1, '*********************************************\n' );
-
-  end
-%
-%  WRITE the data to files.
-%
-  node_filename = 'rectangle_nodes.txt';
-
-  r8mat_write ( node_filename, 2, node_num, node_xy );
-
-  fprintf ( 1, '\n' );
-  fprintf ( 1, '  Wrote the node file "%s"\n', node_filename );
-
-  element_filename = 'rectangle_elements.txt';
-
-  i4mat_write ( element_filename, element_order, element_num, element_node );
-
-  fprintf ( 1, '  Wrote the element file "%s"\n', element_filename );
-
-  value_filename = 'rectangle_solution.txt';
-
-  r8mat_write ( value_filename, 1, node_num, c' );
-
-  fprintf ( 1, '  Wrote the solution value file "%s"\n', value_filename );
-%
-%  Terminate.
-%
-  fprintf ( 1, '\n' );
-  fprintf ( 1, 'FEM2D_POISSON_RECTANGLE_LINEAR:\n' );
-  fprintf ( 1, '  Normal end of execution.\n' );
-  fprintf ( 1, '\n' );
-  timestamp ( );
-
-  return
-end
-function [ u, dudx, dudy ] = exact ( x, y )
-
-%*****************************************************************************80
-%
-%% EXACT calculates the exact solution and its first derivatives.
-%
-%  Discussion:
-%
-%    The function specified here depends on the problem being
-%    solved.  The user must be sure to change both EXACT and RHS
-%    or the program will have inconsistent data.
-%
-%  Licensing:
-%
-%    This code is distributed under the GNU LGPL license.
-%
-%  Modified:
-%
-%    28 November 2008
-%
-%  Author:
-%
-%    John Burkardt
-%
-%  Parameters:
-%
-%    Input, real X, Y, the coordinates of a point
-%    in the region, at which the exact solution is to be evaluated.
-%
-%    Output, real U, DUDX, DUDY, the value of
-%    the exact solution U and its derivatives dUdX
-%    and dUdY at the point (X,Y).
-%
-  u    =      sin ( pi * x ) * sin ( pi * y ) + x;
-  dudx = pi * cos ( pi * x ) * sin ( pi * y ) + 1.0;
-  dudy = pi * sin ( pi * x ) * cos ( pi * y );
-
-  return
-end
-function i4mat_write ( output_filename, m, n, table )
-
-%*****************************************************************************80
-%
-%% I4MAT_WRITE writes an I4MAT file.
-%
-%  Licensing:
-%
-%    This code is distributed under the GNU LGPL license.
-%
-%  Modified:
-%
-%    09 August 2009
-%
-%  Author:
-%
-%    John Burkardt
-%
-%  Parameters:
-%
-%    Input, string OUTPUT_FILENAME, the output filename.
-%
-%    Input, integer M, the spatial dimension.
-%
-%    Input, integer N, the number of points.
-%
-%    Input, integer TABLE(M,N), the points.
-%
-
-%
-%  Open the file.
-%
-  output_unit = fopen ( output_filename, 'wt' );
-
-  if ( output_unit < 0 )
-    fprintf ( 1, '\n' );
-    fprintf ( 1, 'I4MAT_WRITE - Error!\n' );
-    fprintf ( 1, '  Could not open the output file.\n' );
-    error ( 'I4MAT_WRITE - Error!' );
-  end
-%
-%  Write the data.
-%
-  for j = 1 : n
-    for i = 1 : m
-      fprintf ( output_unit, '  %12d', round ( table(i,j) ) );
-    end
-    fprintf ( output_unit, '\n' );
-  end
-%
-%  Close the file.
-%
-  fclose ( output_unit );
-
-  return
-end
-function r8mat_write ( output_filename, m, n, table )
-
-%*****************************************************************************80
-%
-%% R8MAT_WRITE writes an R8MAT file.
-%
-%  Licensing:
-%
-%    This code is distributed under the GNU LGPL license.
-%
-%  Modified:
-%
-%    11 August 2009
-%
-%  Author:
-%
-%    John Burkardt
-%
-%  Parameters:
-%
-%    Input, string OUTPUT_FILENAME, the output filename.
-%
-%    Input, integer M, the spatial dimension.
-%
-%    Input, integer N, the number of points.
-%
-%    Input, real TABLE(M,N), the points.
-%
-
-%
-%  Open the file.
-%
-  output_unit = fopen ( output_filename, 'wt' );
-
-  if ( output_unit < 0 )
-    fprintf ( 1, '\n' );
-    fprintf ( 1, 'R8MAT_WRITE - Error!\n' );
-    fprintf ( 1, '  Could not open the output file.\n' );
-    error ( 'R8MAT_WRITE - Error!' );
-  end
-%
-%  Write the data.
-%
-%  For smaller data files, and less precision, try:
-%
-%     fprintf ( output_unit, '  %14.6f', table(i,j) );
-%
-  for j = 1 : n
-    for i = 1 : m
-      fprintf ( output_unit, '  %24.16f', table(i,j) );
-    end
-    fprintf ( output_unit, '\n' );
-  end
-%
-%  Close the file.
-%
-  fclose ( output_unit );
-
-  return
-end
-function timestamp ( )
-
-%*****************************************************************************80
-%
-%% TIMESTAMP prints the current YMDHMS date as a timestamp.
-%
-%  Licensing:
-%
-%    This code is distributed under the GNU LGPL license.
-%
-%  Modified:
-%
-%    14 February 2003
-%
-%  Author:
-%
-%    John Burkardt
-%
-  t = now;
-  c = datevec ( t );
-  s = datestr ( c, 0 );
-  fprintf ( 1, '%s\n', s );
-
-  return
-end
+% %  SOLVE the linear system A * C = B.
+% %
+%   c = a \ b;
+% %
+% %  COMPARE computed and exact solutions at the nodes.
+% %
+%   fprintf ( 1, '\n' );
+%   fprintf ( 1, '     K     I     J          X           Y        U               U\n' );
+%   fprintf ( 1, '                                                 exact           computed \n' );
+% 
+%   k = 0;
+% 
+%   for j = 1 : ny
+%     fprintf ( 1, '\n' );
+%     for i = 1 : nx
+% 
+%       k = k + 1;
+% 
+%       [ u, dudx, dudy ] = exact ( node_xy(1,k), node_xy(2,k) );
+% 
+%       fprintf ( 1, '  %4d  %4d  %4d  %10f  %10f  %14e  %14e  %14e\n', ...
+%         k, i, j, node_xy(1,k), node_xy(2,k), u, c(k), abs ( u - c(k) ) );
+% 
+%     end
+% 
+%   end
+% %
+% %  Compute error integrals.
+% %
+%   if ( 1 )
+% 
+%   el2 = 0.0;
+%   eh1 = 0.0;
+% 
+%   for e = 1 : element_num
+% 
+%     i1 = element_node(1,e);
+%     i2 = element_node(2,e);
+%     i3 = element_node(3,e);
+% 
+%     area = 0.5 * ...
+%       ( node_xy(1,i1) * ( node_xy(2,i2) - node_xy(2,i3) ) ...
+%       + node_xy(1,i2) * ( node_xy(2,i3) - node_xy(2,i1) ) ...
+%       + node_xy(1,i3) * ( node_xy(2,i1) - node_xy(2,i2) ) );
+% %
+% %  Consider each quadrature point.
+% %  Here, we use the midside nodes as quadrature points.
+% %
+%     for q1 = 1 : 3
+% 
+%       q2 = mod ( q1, 3 ) + 1;
+% 
+%       nq1 = element_node(q1,e);
+%       nq2 = element_node(q2,e);
+% 
+%       xq = 0.5 * ( node_xy(1,nq1) + node_xy(1,nq2) );
+%       yq = 0.5 * ( node_xy(2,nq1) + node_xy(2,nq2) );
+%       wq = 1.0 / 3.0;
+% 
+%       uh = 0.0;
+%       dudxh = 0.0;
+%       dudyh = 0.0;
+% 
+%       for tj1 = 1 : element_order
+% 
+%         tj2 = mod ( tj1,     3 ) + 1;
+%         tj3 = mod ( tj1 + 1, 3 ) + 1;
+% 
+%         ntj1 = element_node(tj1,e);
+%         ntj2 = element_node(tj2,e);
+%         ntj3 = element_node(tj3,e);
+% 
+%         qj = 0.5 * ( ...
+%             ( node_xy(1,ntj3) - node_xy(1,ntj2) ) * ( yq - node_xy(2,ntj2) ) ...
+%           - ( node_xy(2,ntj3) - node_xy(2,ntj2) ) * ( xq - node_xy(1,ntj2) ) ) ...
+%               / area;
+%         dqjdx = - 0.5 * ( node_xy(2,ntj3) - node_xy(2,ntj2) ) / area;
+%         dqjdy =   0.5 * ( node_xy(1,ntj3) - node_xy(1,ntj2) ) / area;
+% 
+%         uh    = uh    + c(ntj1) * qj;
+%         dudxh = dudxh + c(ntj1) * dqjdx;
+%         dudyh = dudyh + c(ntj1) * dqjdy;
+% 
+%       end
+% 
+%       [ u, dudx, dudy ] = exact ( xq, yq );
+% 
+%       el2 = el2 + ( uh - u )^2 * area;
+%       eh1 = eh1 + ( ( dudxh - dudx )^2 + ( dudyh - dudy )^2 ) * area;
+% 
+%     end
+% 
+%   end
+% 
+%   el2 = sqrt ( el2 );
+%   eh1 = sqrt ( eh1 );
+% 
+%   fprintf ( 1, '\n' );
+%   fprintf ( 1, '*********************************************\n' );
+%   fprintf ( 1, '*                                           *\n' );
+%   fprintf ( 1, '*  ERRORS:                                  *\n' );
+%   fprintf ( 1, '*    L2 error =          %14f     *\n', el2 );
+%   fprintf ( 1, '*    H1-seminorm error = %14f     *\n', eh1 );
+%   fprintf ( 1, '*                                           *\n' );
+%   fprintf ( 1, '*********************************************\n' );
+% 
+%   end
+% %
+% %  WRITE the data to files.
+% %
+%   node_filename = 'rectangle_nodes.txt';
+% 
+%   r8mat_write ( node_filename, 2, node_num, node_xy );
+% 
+%   fprintf ( 1, '\n' );
+%   fprintf ( 1, '  Wrote the node file "%s"\n', node_filename );
+% 
+%   element_filename = 'rectangle_elements.txt';
+% 
+%   i4mat_write ( element_filename, element_order, element_num, element_node );
+% 
+%   fprintf ( 1, '  Wrote the element file "%s"\n', element_filename );
+% 
+%   value_filename = 'rectangle_solution.txt';
+% 
+%   r8mat_write ( value_filename, 1, node_num, c' );
+% 
+%   fprintf ( 1, '  Wrote the solution value file "%s"\n', value_filename );
+% %
+% %  Terminate.
+% %
+%   fprintf ( 1, '\n' );
+%   fprintf ( 1, 'FEM2D_POISSON_RECTANGLE_LINEAR:\n' );
+%   fprintf ( 1, '  Normal end of execution.\n' );
+%   fprintf ( 1, '\n' );
+%   timestamp ( );
+% 
+%   return
+% end
+% function [ u, dudx, dudy ] = exact ( x, y )
+% 
+% %*****************************************************************************80
+% %
+% %% EXACT calculates the exact solution and its first derivatives.
+% %
+% %  Discussion:
+% %
+% %    The function specified here depends on the problem being
+% %    solved.  The user must be sure to change both EXACT and RHS
+% %    or the program will have inconsistent data.
+% %
+% %  Licensing:
+% %
+% %    This code is distributed under the GNU LGPL license.
+% %
+% %  Modified:
+% %
+% %    28 November 2008
+% %
+% %  Author:
+% %
+% %    John Burkardt
+% %
+% %  Parameters:
+% %
+% %    Input, real X, Y, the coordinates of a point
+% %    in the region, at which the exact solution is to be evaluated.
+% %
+% %    Output, real U, DUDX, DUDY, the value of
+% %    the exact solution U and its derivatives dUdX
+% %    and dUdY at the point (X,Y).
+% %
+%   u    =      sin ( pi * x ) * sin ( pi * y ) + x;
+%   dudx = pi * cos ( pi * x ) * sin ( pi * y ) + 1.0;
+%   dudy = pi * sin ( pi * x ) * cos ( pi * y );
+% 
+%   return
+% end
+% function i4mat_write ( output_filename, m, n, table )
+% 
+% %*****************************************************************************80
+% %
+% %% I4MAT_WRITE writes an I4MAT file.
+% %
+% %  Licensing:
+% %
+% %    This code is distributed under the GNU LGPL license.
+% %
+% %  Modified:
+% %
+% %    09 August 2009
+% %
+% %  Author:
+% %
+% %    John Burkardt
+% %
+% %  Parameters:
+% %
+% %    Input, string OUTPUT_FILENAME, the output filename.
+% %
+% %    Input, integer M, the spatial dimension.
+% %
+% %    Input, integer N, the number of points.
+% %
+% %    Input, integer TABLE(M,N), the points.
+% %
+% 
+% %
+% %  Open the file.
+% %
+%   output_unit = fopen ( output_filename, 'wt' );
+% 
+%   if ( output_unit < 0 )
+%     fprintf ( 1, '\n' );
+%     fprintf ( 1, 'I4MAT_WRITE - Error!\n' );
+%     fprintf ( 1, '  Could not open the output file.\n' );
+%     error ( 'I4MAT_WRITE - Error!' );
+%   end
+% %
+% %  Write the data.
+% %
+%   for j = 1 : n
+%     for i = 1 : m
+%       fprintf ( output_unit, '  %12d', round ( table(i,j) ) );
+%     end
+%     fprintf ( output_unit, '\n' );
+%   end
+% %
+% %  Close the file.
+% %
+%   fclose ( output_unit );
+% 
+%   return
+% end
+% function r8mat_write ( output_filename, m, n, table )
+% 
+% %*****************************************************************************80
+% %
+% %% R8MAT_WRITE writes an R8MAT file.
+% %
+% %  Licensing:
+% %
+% %    This code is distributed under the GNU LGPL license.
+% %
+% %  Modified:
+% %
+% %    11 August 2009
+% %
+% %  Author:
+% %
+% %    John Burkardt
+% %
+% %  Parameters:
+% %
+% %    Input, string OUTPUT_FILENAME, the output filename.
+% %
+% %    Input, integer M, the spatial dimension.
+% %
+% %    Input, integer N, the number of points.
+% %
+% %    Input, real TABLE(M,N), the points.
+% %
+% 
+% %
+% %  Open the file.
+% %
+%   output_unit = fopen ( output_filename, 'wt' );
+% 
+%   if ( output_unit < 0 )
+%     fprintf ( 1, '\n' );
+%     fprintf ( 1, 'R8MAT_WRITE - Error!\n' );
+%     fprintf ( 1, '  Could not open the output file.\n' );
+%     error ( 'R8MAT_WRITE - Error!' );
+%   end
+% %
+% %  Write the data.
+% %
+% %  For smaller data files, and less precision, try:
+% %
+% %     fprintf ( output_unit, '  %14.6f', table(i,j) );
+% %
+%   for j = 1 : n
+%     for i = 1 : m
+%       fprintf ( output_unit, '  %24.16f', table(i,j) );
+%     end
+%     fprintf ( output_unit, '\n' );
+%   end
+% %
+% %  Close the file.
+% %
+%   fclose ( output_unit );
+% 
+%   return
+% end
+% function timestamp ( )
+% 
+% %*****************************************************************************80
+% %
+% %% TIMESTAMP prints the current YMDHMS date as a timestamp.
+% %
+% %  Licensing:
+% %
+% %    This code is distributed under the GNU LGPL license.
+% %
+% %  Modified:
+% %
+% %    14 February 2003
+% %
+% %  Author:
+% %
+% %    John Burkardt
+% %
+%   t = now;
+%   c = datevec ( t );
+%   s = datestr ( c, 0 );
+%   fprintf ( 1, '%s\n', s );
+% 
+%   return
+% end
